9643e22fd9c58bd8b6609afdc97249298f3331ed
Postmodern <postmodern.mod3@gmail.com>
2012-04-05 18:56:51 -0700

    Style.

--- lib/ffi/libnet/stats.rb
+++ lib/ffi/libnet/stats.rb
@@ -3,9 +3,11 @@ require 'ffi'
 module FFI
   module Libnet
     class Stats < FFI::Struct
+
       layout :packets_sent, :int64,
              :packet_errors, :int64,
              :bytes_written, :int64
+
     end
   end
 end
